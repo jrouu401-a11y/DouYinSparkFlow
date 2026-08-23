00457a2691c5f500fdf4e9eefca5e6428adaba5b
jrouu401-a11y <jrouu401@gmail.com>
2026-08-23 23:46:41 +0800

    docs: add shareable deployment launcher

---
+++ 一键部署.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+rem Opens the three setup pages required for a recipient's own deployment.
+start "" "https://github.com/jrouu401-a11y/DouYinSparkFlow/fork"
+start "" "https://oilu.cn/DouYinSparkFlow/"
+start "" "https://github.com/jrouu401-a11y/DouYinSparkFlow/blob/main/docs/Action%E9%83%A8%E7%BD%B2%E8%AF%B4%E6%98%8E.md"
+
+endlocal
